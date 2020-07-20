1040060848e3deaffe68befdb179ff0fbd8092a5
Alexandre de Souza <alexandre@aledsz.com.br>
2020-07-19 22:32:36 -0300

    Add StylistEx module

---
+++ lib/stylist_ex.ex
@@ -0,0 +1,7 @@
+defmodule StylistEx do
+  @moduledoc """
+  StylistEx is a parser for CSS to Elixir types and structs,
+  which can generate CSS string with validations that are triggered
+  before "compiling the CSS".
+  """
+end
