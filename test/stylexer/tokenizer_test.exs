2d72f1d36b017960a163da82cc4c209c57c228c6
Alexandre de Souza <alexandre@aledsz.com.br>
2020-07-27 14:39:57 -0300

    Add tests with hexadecimal

--- test/stylexer/tokenizer_test.exs
+++ test/stylexer/tokenizer_test.exs
@@ -11,5 +11,19 @@ defmodule Stylexer.TokenizerTest do
     test "digits should be parsed" do
       assert {:ok, [{:digit_token, 1, '123'}]} === Tokenizer.consume("123")
     end
+
+    test "hex digits should be parsed" do
+      assert {:ok, [
+        {:hash_token, 1, '#'},
+        {:escaped_token, 1, 'bbaa'},
+        {:digit_token, 1, '13'}
+      ]} === Tokenizer.consume("#bbaa13")
+
+      assert {:ok, [
+        {:hash_token, 1, '#'},
+        {:escaped_token, 1, 'Bbaa'},
+        {:digit_token, 1, '13'}
+      ]} === Tokenizer.consume("#Bbaa13")
+    end
   end
 end
