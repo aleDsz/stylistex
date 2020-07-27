ed6a15fe3ae73e4572a8c2afc32ad5a59e5ed5b7
Alexandre de Souza <alexandre@aledsz.com.br>
2020-07-27 16:46:05 -0300

    Add more tests with Lexer

--- test/stylexer/tokenizer_test.exs
+++ test/stylexer/tokenizer_test.exs
@@ -25,5 +25,64 @@ defmodule Stylexer.TokenizerTest do
         {:digit_token, 1, '13'}
       ]} === Tokenizer.consume("#Bbaa13")
     end
+
+    test "multline commentaries should be parsed" do
+      comment =
+        """
+        /*
+         * test
+         */
+        """
+      
+      assert {:ok, [{:commentary_token, 1, '/*\n * test\n */'}]} === Tokenizer.consume(comment)
+    end
+
+    test "inline commentaries should be parsed" do
+      comment = "/* test */"
+      assert {:ok, [{:commentary_token, 1, '/* test */'}]} === Tokenizer.consume(comment)
+    end
+
+    test "multiline css definition should be parsed" do
+      css = 
+        """
+        body {
+          background: #ffaa12;
+        }
+        """
+      
+      assert {:ok, [
+        {:escaped_token, 1, 'body'},
+        {:left_curly_token, 1, '{'},
+        {:escaped_token, 2, 'background'},
+        {:colon_token, 2, ':'},
+        {:hash_token, 2, '#'},
+        {:escaped_token, 2, 'ffaa'},
+        {:digit_token, 2, '12'},
+        {:semicolon_token, 2, ';'},
+        {:right_curly_token, 3, '}'}
+      ]} === Tokenizer.consume(css)
+    end
+
+    test "inline css definition should be parsed" do
+      css = "body { background: #ffaa12; }"
+      
+      assert {:ok, [
+        {:escaped_token, 1, 'body'},
+        {:left_curly_token, 1, '{'},
+        {:escaped_token, 1, 'background'},
+        {:colon_token, 1, ':'},
+        {:hash_token, 1, '#'},
+        {:escaped_token, 1, 'ffaa'},
+        {:digit_token, 1, '12'},
+        {:semicolon_token, 1, ';'},
+        {:right_curly_token, 1, '}'}
+      ]} === Tokenizer.consume(css)
+    end
+
+    test "function token should be parsed" do
+      css = "url(http://localhost/img.jpg)"
+      
+      assert {:ok, [{:function_token, 1, 'url(http://localhost/img.jpg)'}]} === Tokenizer.consume(css)
+    end
   end
 end
